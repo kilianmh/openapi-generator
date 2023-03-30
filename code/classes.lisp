e900e53f212665d600d68738b1b0360053fb1a12
kilianmh <kilian.haemmerle@protonmail.com>
2023-03-30 17:27:23 +0200

    Fix: request-body content slot media-type
    Class request-body content slot is now hash-table with value type
    media-type instead of content. This is correct according to the
    Openapi standard.

--- code/classes.lisp
+++ code/classes.lisp
@@ -260,7 +260,7 @@
                             :documentation "A brief description of the request body. This could contain examples of use. CommonMark syntax MAY be used for rich text representation.")
              ("required" :any
                          :documentation "Determines if the request body is required in the request. Defaults to false.") ;; bool
-             ("content"  (:hash-table content)
+             ("content"  (:hash-table media-type)
                          :documentation "REQUIRED. The content of the request body. The key is a media type or media type range and the value describes it. For requests that match multiple keys, only the most specific key is applicable. e.g. text/plain overrides text/*")))
 
 (defmethod print-object ((object request-body) stream)
