50eb5a75e0b23851fcf81a3564fbe3ad389e24ca
kilianmh <kilian.haemmerle@protonmail.com>
2023-12-05 14:40:32 +0100

    Tests: test generated system
    Now the codeberg system is generated and one exemplary function
    called to test if it is working. Working network connection is
    required for execution of the tests.

--- test/test.lisp
+++ test/test.lisp
@@ -1,12 +1,16 @@
-(defpackage #:openapi-generator/test
-  (:use #:cl
-        #:openapi-generator
-        #:fiveam)
+(uiop:define-package #:openapi-generator/test
+  (:use #:cl)
+  (:import-from #:fiveam
+		#:def-suite
+		#:def-test
+		#:finishes
+		#:is-true)
   (:shadowing-import-from #:str
                           #:concat
                           #:downcase)
   (:import-from #:openapi-generator
-                #:parse-openapi)
+                #:parse-openapi
+		#:make-openapi-client)
   (:shadow #:type)
   (:export #:run!
            #:test-suite))
@@ -16,6 +20,13 @@
 (def-suite test-suite :description "Main Test Suite for Openapi-generator")
 
 (def-test parsing (:suite test-suite)
-  (finishes (openapi-generator:parse-openapi "openai"
+  (finishes (parse-openapi "openai"
              :url "https://raw.githubusercontent.com/hirosystems/stacks-blockchain-api/gh-pages/openapi.resolved.yaml")))
 
+
+(def-test make-openapi-client (:suite test-suite)
+  (finishes (make-openapi-client "codeberg"
+				 :url "https://codeberg.org/swagger.v1.json"
+				 :server "https://codeberg.org/api/v1"
+				 :parse t))
+  (is-true (funcall (uiop:find-symbol* '#:repo-get '#:codeberg) "kilianmh" "openapi-generator")))
